55ddf78e4411e336d55776bc08cf538bad32a286
Sanket Dhengre <sanketdhengre@gmail.com>
2025-05-27 02:53:40 +0530

    worked more on the validation part

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
-const {listingSchema, reviewSchema} = require("./schema.js");
+const { listingSchema, reviewSchema } = require("./schema.js");
 const Review = require("./models/review.js");
 
 app.use(methodOverride("_method"));
@@ -39,11 +39,11 @@ const validateListing = (req, res, next) => {
   const { error } = listingSchema.validate(req.body);
   if (error) {
     const msg = error.details.map((el) => el.message).join(",");
-    throw new ExpressError(msg);
+    throw new ExpressError(400, msg);
   } else {
     next();
   }
-}
+};
 
 const validateReview = (req, res, next) => {
   const { error } = reviewSchema.validate(req.body);
@@ -53,7 +53,7 @@ const validateReview = (req, res, next) => {
   } else {
     next();
   }
-}
+};
 
 //Index Route
 app.get("/listings", async (req, res) => {
@@ -74,11 +74,15 @@ app.get("/listings/:id", async (req, res) => {
 });
 
 //Create Route
-app.post("/listings", async (req, res) => {
-  const newListing = new Listing(req.body.listing);
-  await newListing.save();
-  res.redirect("/listings");
-});
+app.post(
+  "/listings",
+  validateListing,
+  wrapAsync(async (req, res, next) => {
+    const newListing = new Listing(req.body.listing);
+    await newListing.save();
+    res.redirect("/listings");
+  })
+);
 
 //Edit Route
 app.get("/listings/:id/edit", async (req, res) => {
@@ -88,11 +92,15 @@ app.get("/listings/:id/edit", async (req, res) => {
 });
 
 //Update Route
-app.put("/listings/:id", async (req, res) => {
-  const { id } = req.params;
-  await Listing.findByIdAndUpdate(id, {...req.body.listing });
-  res.redirect(`/listings/${id}`);
-});
+app.put(
+  "/listings/:id",
+  validateListing,
+  wrapAsync(async (req, res, next) => {
+    const { id } = req.params;
+    await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+    res.redirect(`/listings/${id}`);
+  })
+);
 
 //Delete Route
 app.delete("/listings/:id", async (req, res) => {
@@ -104,31 +112,36 @@ app.delete("/listings/:id", async (req, res) => {
 //REVIEWS
 //POST review Route
 
-app.post("/listings/:id/reviews", validateReview, wrapAsync (async (req, res) => {
-  let listing = await Listing.findById(req.params.id);
-  let newReview = new Review(req.body.review);
+app.post(
+  "/listings/:id/reviews",
+  validateReview,
+  wrapAsync(async (req, res) => {
+    let listing = await Listing.findById(req.params.id);
+    let newReview = new Review(req.body.review);
 
-  listing.reviews.push(newReview);
+    listing.reviews.push(newReview);
 
-  await newReview.save();
-  await listing.save();
+    await newReview.save();
+    await listing.save();
 
-  console.log("Review saved to database");
-  res.send("new review added successfully");
+    console.log("Review saved to database");
+    res.send("new review added successfully");
 
-  res.redirect(`/listings/${listing._id}`);
-}));
+    res.redirect(`/listings/${listing._id}`);
+  })
+);
 
 //DELETE review Route
-app.delete("/listings/:id/reviews/:reviewId", wrapAsync(async (req, res) => {
+app.delete(
+  "/listings/:id/reviews/:reviewId",
+  wrapAsync(async (req, res) => {
+    let { id, reviewId } = req.params;
+    await Listing.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
+    await Review.findByIdAndDelete(reviewId);
 
-  let { id, reviewId } = req.params;
-  await Listing.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
-  await Review.findByIdAndDelete(reviewId);
-
-  res.redirect(`/listings/${id}`);
-
-}))
+    res.redirect(`/listings/${id}`);
+  })
+);
 
 // app.get("/testListings", async (req, res) => {
 //     let sampleListing = new Listing({
@@ -144,6 +157,13 @@ app.delete("/listings/:id/reviews/:reviewId", wrapAsync(async (req, res) => {
 //     console.log("Listing saved to database");
 //     res.send("successful testing");
 // });
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  let { statusCode = 500, message = "Some Error Occured!" } = err;
+
+  res.status(statusCode).render("error.ejs", { message });
+});
 
 app.listen(8080, () => {
   console.log("Server is running on port 8080");
