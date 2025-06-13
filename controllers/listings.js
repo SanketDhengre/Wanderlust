736c65ecb74abbcbc6e636ddc7ef7da892f59922
Sanket Dhengre <sanketdhengre@gmail.com>
2025-06-14 01:47:50 +0530

    completed the phase 3 of the project and added the map popup and marker functionality in the application

--- controllers/listings.js
+++ controllers/listings.js
@@ -34,15 +34,14 @@ module.exports.createListing = async (req, res, next) => {
     })
     .send();
 
-  console.log(response.body.features[0].geometry);
-  res.send("done");
-
   let url = req.file.path;
   let filename = req.file.filename;
   const newListing = new Listing(req.body.listing);
   newListing.owner = req.user._id; // Set the owner to the currently logged-in user
   newListing.image = { url, filename }; // Set the image URL and filename
-  await newListing.save();
+  newListing.geometry = response.body.features[0].geometry;
+  let savedListing = await newListing.save();
+  console.log(savedListing);
   req.flash("success", "New listing created successfully!");
   res.redirect("/listings");
 };
--- public/js/map.js
+++ public/js/map.js
@@ -3,6 +3,15 @@ mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
   container: "map",
   style: "mapbox://styles/mapbox/streets-v9",
-  center: [30, 15],
+  center: listing.geometry.coordinates,
   zoom: 9,
 });
+
+const marker = new mapboxgl.Marker({ color: "red" })
+  .setLngLat(listing.geometry.coordinates)
+  .setPopup(
+    new mapboxgl.Popup({ offset: 25 }).setHTML(
+      `<h3>${listing.title}</h3> <p> Exact location will be provided upon booking.</p>`
+    )
+  )
+  .addTo(map);
